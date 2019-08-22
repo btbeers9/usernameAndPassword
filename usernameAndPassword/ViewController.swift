4333a407b5e42d5e898a4f8bfd23ad57fa39c3a4
Brian <brianbeers7@gmail.com>
2019-08-21 20:39:28 -0500

    i did something

--- usernameAndPassword/ViewController.swift
+++ usernameAndPassword/ViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func enterButtonPressed(_ sender: Any) {
+        
+        if usernameTextField.text == "btbeers" && passwordTextField.text == "qwerty" {
+            resultLabel.text = "Correct!"
+            
+            usernameTextField.resignFirstResponder()
+            passwordTextField.resignFirstResponder()
+            
+        } else {
+            resultLabel.text = "Incorrect!"
+        }
+        
+    }
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
